4af2c5f5f5665b376a29725b47adf07f259db9d6
Alberto Fernández <albertof.rubio@colaboradoresunidadeditorial.es>
2017-01-16 14:55:10 +0100

    First code commit.

---
+++ app/src/main/java/com/bidigames/viewpagersample/BlankFragment.java
@@ -0,0 +1,47 @@
+package com.bidigames.viewpagersample;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+    // Store instance variables
+    private String title;
+    private int page;
+
+    // newInstance constructor for creating fragment with arguments
+    public static BlankFragment newInstance(int page, String title) {
+        BlankFragment fragmentFirst = new BlankFragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        fragmentFirst.setArguments(args);
+        return fragmentFirst;
+    }
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someTitle");
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel);
+        tvLabel.setText(page + " -- " + title);
+        return view;
+    }
+}
